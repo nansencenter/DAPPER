83596bf096821cb4a3d6f1d60cef8b4113c38107
patricknraanes <patrick.n.raanes@gmail.com>
2016-07-04 13:14:52 +0200

    Updated (mainly doc) of stochastic module

--- stoch.py
+++ stoch.py
@@ -2,12 +2,13 @@
 
 from common import *
 
-
-# (Unit) random number generator for x-platform use
-# (since code is easy to translate).
-# "Linear congruential generator"
-# Should be burnt-in by running it a few times.
 def LCG(seed=-1):
+  """
+  (Unit) random number generator for X-platform use
+  (since code is easy to translate).
+  "Linear congruential generator"
+  Should be burnt-in by running it a few times.
+  """  
   if seed > -1:
     LCG.k = seed
   m = 2**32
@@ -17,47 +18,57 @@ def LCG(seed=-1):
   return float(LCG.k) / m
 LCG.k = 1
 
-# Unit pseudo-random numbers ucing LCG
 def myrand(shape=(1,)):
+  """Unit pseudo-random numbers via LCG."""
   N = prod(shape)
   rand_U = [LCG() for k in range(N)]
   return reshape(rand_U, shape)
 
-# Approximately Gaussian N(0,1),
-# using logit transform of U(0,1) vars (from LCG)
 def myrandn(shape=(1,)):
+  """
+  Approximately Gaussian N(0,1).
+  Using logit transform of U(0,1) vars (from LCG).
+  """  
   u = myrand(shape)
   return sqrt(pi/8.) * log(u/(1-u))
 
-# Built-in Gaussian generator
+# Built-in generators
 def randn(shape=(1,)): return np.random.normal(0,1,shape)
-# Use LCG for all randn
-#randn = myrandn
-
 def rand(shape=(1,)): return np.random.uniform(0,1,shape)
 
-# Makeshift randcorr (which is missing from rogues)
+# Use LCG for all randn
+#def randn(*kargs):
+  #raise Warning('Using barebones LCG random numbers')
+  #return myrandn(*kargs)
+#def rand(*kargs):
+  #raise Warning('Using barebones LCG random numbers')
+  #return myrand(*kargs)
+
 def randcov(m):
+  """(Makeshift) random cov mat
+  (which is missing from rogues)"""
   N = ceil(2+m**1.2)
   E = randn((N,m))
   return E.T @ E
 def randcorr(m):
+  """(Makeshift) random corr mat
+  (which is missing from rogues)"""
   Cov  = randcov(m)
   Dm12 = diag(diag(Cov)**(-0.5))
   return Dm12@Cov@Dm12
 
 
-# Generate random orthonormal matrix:
 def genOG(m):
+  """Generate random orthonormal matrix."""
   Q,R = nla.qr(randn((m,m)))
   for i in range(m):
     if R[i,i] < 0:
       Q[:,i] = -Q[:,i]
   return Q
 
-# Random orthonormal mean-preserving matrix:
-# Source: ienks code of Sakov/Bocquet
 def genOG_1(N):
+  """Random orthonormal mean-preserving matrix.
+  Source: ienks code of Sakov/Bocquet."""
   e = ones((N,1))
   V = nla.svd(e)[0] # Basis whose first vector is e
   Q = genOG(N-1)     # Orthogonal mat
