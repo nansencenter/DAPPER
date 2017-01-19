ff76136d2f09dbd8e2acdc96407f3f6e2ebbc15f
patricknraanes <patrick.n.raanes@gmail.com>
2017-01-19 11:11:54 +0100

    Moved m33 to raanes2016. [Wasnt this done before?]

---
+++ mods/Lorenz95/raanes2016.py
@@ -0,0 +1,13 @@
+# Reproduce raanes'2016 ("EnRTS and EnKS")
+
+from common import *
+
+from mods.Lorenz95.sak08 import setup
+
+setup.t = Chronology(0.01,dkObs=15,T=4**5,BurnIn=20)
+setup.name = os.path.relpath(__file__,'mods/')
+
+
+#from mods.Lorenz95.raanes2016 import setup
+#config = DAC(EnKS ,'Sqrt',N=25,infl=1.08,rot=False,tLag=2.0)
+#config = DAC(EnRTS,'Sqrt',N=25,infl=1.08,rot=False,cntr=0.99)
