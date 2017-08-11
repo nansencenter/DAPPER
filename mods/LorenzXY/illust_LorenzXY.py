d72e396f8700b64d98a88a38b3e580b843d16d7b
patricknraanes <patrick.n.raanes@gmail.com>
2017-08-11 11:56:00 +0200

    Updated illust_LorenzXY

--- mods/LorenzXY/illust_LorenzXY.py
+++ mods/LorenzXY/illust_LorenzXY.py
@@ -1,65 +1,58 @@
-from common import *
-sd0 = seed(4)
+# Quick illustration.
+# Sorry for the mess.
 
+from common import *
+from matplotlib import cm
 plt.style.use('AdInf/paper.mplstyle')
 
-
+# Setup
+sd0 = seed(4)
 from mods.LorenzXY.core import nX,J,m
 from mods.LorenzXY.defaults import setup
-K = 1000
-setup.t.BurnIn = 2
-setup.t.K      = K
+dt = 0.005
+setup.t = Chronology(dt,dt,round2(6,dt))
 xx,yy = simulate(setup)
 
+# Grab parts of state vector
 ii = arange(nX+1)
 jj = arange(nX*J+1)
 circX = np.mod(ii  ,nX)
 circY = np.mod(jj,nX*J) + nX
+iX = np.hstack([0, 0.5+arange(8), 8])
+def Xi(xx):
+  interp = (xx[0]+xx[-1])/2
+  return np.hstack([interp, xx, interp])
 
 # Animate linear
-#plt.figure(1)
-#lhX   = plt.plot(arange(nX+1)    ,xx[-1][circX],'b',lw=3)[0]
-#lhY   = plt.plot(arange(nX*J+1)/J,xx[-1][circY],'g',lw=2)[0]
-#for k in progbar(range(K),'Plotting'):
-  #lhX.set_ydata(xx[k][circX])
-  #lhY.set_ydata(xx[k][circY])
-  #plt.pause(0.001)
+plt.figure(1)
+lhX   = plt.plot(arange(nX+1)    ,xx[-1][circX],'b',lw=3)[0]
+lhY   = plt.plot(arange(nX*J+1)/J,xx[-1][circY],'g',lw=2)[0]
+for k in progbar(range(setup.t.K),'Plotting'):
+  lhX.set_ydata(xx[k][circX])
+  lhY.set_ydata(xx[k][circY])
+  plt.pause(0.001)
 
 # Overlay linear
-from matplotlib import cm
 fg = plt.figure(2)
 fg.clear()
 ax = fg.gca()
-#ax.set_axis_off()
-ax.set_xticks(arange(nX+1))
-ax.set_xlabel('Y-index')
-ax.set_xticklabels(['1'] + [str((i+1)*J) for i in circX])
-ax2 = ax.twiny()
-ax2.set_xlabel('X-index')
-ax2.set_xticklabels([str(i+1) for i in circX])
-#ax2.set_xticks(arange(nX+1))
-L = 40 # Num of lines to plot
+tY = arange(nX+1)
+lY = ['1'] + [str((i+1)*J) for i in circX]
+tX = iX[1:-1]
+lX = np.array(['\n'+str(i+1) for i in range(nX)])
+ax.set_xticks(tY)
+ax.set_xticklabels(lY)
+for t, l in zip(tX,lX):
+  ax.text(t-0.05,-6.5,l,fontsize=mpl.rcParams['xtick.labelsize'])
+ax.grid(color='k',alpha=0.6,lw=0.4,axis='both',which='major')
+L = 30 # Num of lines to plot
 for p in range(L):
-  k = 640 + p*1
+  k = int(2e5*dt) + p*2
   c = cm.viridis(1-p/L)
   a = 0.8-0.2*p/L
-  plt.plot(ii  ,xx[k][circX],color=c,lw=2,alpha=a)[0]
-  plt.plot(jj/J,xx[k][circY],color=c,lw=1,alpha=a)[0]
+  plt.plot(iX  ,Xi(xx[k][:nX]),color=c,lw=2  ,alpha=a)[0]
+  plt.plot(jj/J,xx[k][circY]  ,color=c,lw=0.7,alpha=a)[0]
 ax.set_ylim(-5,13)
-
-# Double tick marks
-# Basic
-#ax.set_xticklabels([(str(i) + '/\n' + str(i*J)) for i in circX])
-# Advanced
-#labels = ['|1\n|1']
-#for i in circX[1:-1]:
-  #xi = i+1
-  #yi = i*J
-  #s = u"\u035F"*int(1+log10(yi+0.0001)) + '|' + str(xi) + '\n' + str(yi) + '|'+u"\u035E"
-  #labels.append(s)
-#labels.append(u"\u035F"*2+'|1\n256|'+u"\u035E")
-#ax.set_xticklabels(labels,family='monospace')
-
 
 # Convert to circular coordinates
 def tX(zz):
@@ -73,17 +66,17 @@ def tY(zz):
 
 
 # Animate circ
-#plt.figure(3)
-#lhX   = plt.plot(*tX(xx[-1][circX]),'b',lw=3)[0]
-#lhY   = plt.plot(*tY(xx[-1][circY]),'g',lw=1)[0]
-#for k in progbar(range(K),'Plotting'):
-  #dataX = tX(xx[k][circX])
-  #dataY = tY(xx[k][circY])
-  #lhX.set_xdata(dataX[0])
-  #lhX.set_ydata(dataX[1])
-  #lhY.set_xdata(dataY[0])
-  #lhY.set_ydata(dataY[1])
-  #plt.pause(0.001)
+plt.figure(3)
+lhX   = plt.plot(*tX(xx[-1][circX]),'b',lw=3)[0]
+lhY   = plt.plot(*tY(xx[-1][circY]),'g',lw=1)[0]
+for k in progbar(range(setup.t.K),'Plotting'):
+  dataX = tX(xx[k][circX])
+  dataY = tY(xx[k][circY])
+  lhX.set_xdata(dataX[0])
+  lhX.set_ydata(dataX[1])
+  lhY.set_xdata(dataY[0])
+  lhY.set_ydata(dataY[1])
+  plt.pause(0.001)
 
 
 # Overlay circ
@@ -98,7 +91,7 @@ ax.set_axis_bgcolor('white')
 ax.set_aspect('equal')
 L = 40 # Num of lines to plot
 for p in range(L):
-  k = 200 + p*1
+  k = 143 + p*3
   c = cm.viridis(1-p/L)
   a = 0.8-0.2*p/L
   plt.plot(*tX(xx[k][circX]),color=c,lw=2,alpha=a)[0]
