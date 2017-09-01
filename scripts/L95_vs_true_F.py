0a4389201daaf7733a74f2511a0ce362a0e200d8
patricknraanes <patrick.n.raanes@gmail.com>
2017-09-01 09:30:04 +0200

    Minor upgrades to experiment scripts

--- scripts/L95_vs_true_F.py
+++ scripts/L95_vs_true_F.py
@@ -13,11 +13,11 @@ sd0 = seed(5)
 from mods.Lorenz95.sak08 import setup
 import mods.Lorenz95.core as L95
 
-F_DA    = 8.0
-F_range = arange(6,12)
+F_DA     = 8.0
+settings = arange(6,12) # Forcing F
 
 setup.t.T = 4**4.5
-nRepeat   = 5
+nRepeat   = 8
 
 ############################
 # DA methods
@@ -26,33 +26,31 @@ cfgs = List_of_Configs()
 
 cfgs += Climatology()
 cfgs += Var3D()
-#cfgs += ExtKF(infl=6)
-cfgs += EnKF_N    (N=25,rot=False)
-cfgs += EnKF_AdInf(N=25,rot=False,Nb=1000,Fb=(1-1/40/6),br=0.05)
+cfgs += ExtKF(infl=6)
+cfgs += EnKF_N(N=25,rot=False)
 
-# Not tuned
 #cfgs.assign_names()
 
 ############################
 # Assimilate
 ############################
-avrgs = np.empty((len(F_range),nRepeat,len(cfgs)),dict)
+avrgs = np.empty((len(settings),nRepeat,len(cfgs)),dict)
 stats = np.empty_like(avrgs)
 
-for i,F_true in enumerate(F_range):
-  print_c('\nF_true: ', F_true)
-  for j in range(nRepeat):
-    seed(sd0+j)
-    L95.Force = F_true
+for iS,Setting in enumerate(settings):
+  print_c('\nF_true: ', Setting)
+  for iR in range(nRepeat):
+    seed(sd0+iR)
+    L95.Force = Setting
     xx,yy     = simulate(setup)
     L95.Force = F_DA
-    for k,config in enumerate(cfgs):
-      seed(sd0+j)
-      stats[i,j,k] = config.assimilate(setup,xx,yy)
-      avrgs[i,j,k] = stats[i,j,k].average_in_time()
-    print_averages(cfgs,avrgs[i,j])
-  avrg = average_each_field(avrgs[i],axis=0)
+    for iC,Config in enumerate(cfgs):
+      seed(sd0+iR)
+      stats[iS,iR,iC] = Config.assimilate(setup,xx,yy)
+      avrgs[iS,iR,iC] = stats[iS,iR,iC].average_in_time()
+    print_averages(cfgs,avrgs[iS,iR])
+  avrg = average_each_field(avrgs[iS],axis=0)
   print_c('Average over',nRepeat,'repetitions:')
   print_averages(cfgs,avrg)
 
-#save_data(save_path,inds,F_range=F_range,avrgs=avrgs,xx=xx,yy=yy)
+#save_data(save_path,inds,settings=settings,stng_var=stng_var,avrgs=avrgs,xx=xx,yy=yy)
