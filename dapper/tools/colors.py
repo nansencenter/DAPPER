e7829624af95fe9fac9457bfce46dfa5c10969ce
Patrick <patrick.n.raanes@gmail.com>
2020-10-09 10:07:25 +0200

    Add cmap doc comments

--- dapper/tools/colors.py
+++ dapper/tools/colors.py
@@ -75,3 +75,21 @@ sns_colors = np.array(np.matrix("""
 for code, color in zip('bgrmyckw', sns_colors):
     mpl.colors.colorConverter.colors[code] = color
     mpl.colors.colorConverter.cache [code] = color
+
+
+# MPL colors -- cheat sheet:
+# --------------
+# Equivalent:
+# cmap = plt.get_cmap("tab10")
+# cmap = plt.cm.get_cmap("tab10")
+# cmap = mpl.cm.get_cmap("tab10")
+# cmap = mpl.cm.tab10
+# Equivalent:
+# c = cmap( cmap.N//2 ) # using int
+# c = cmap( .5 )        # using float
+# Equivalent:
+# clist = cmap.colors # (only for ListedColormap) => 2D tuple,
+# clist = cmap(arange(0,.9,.1))                 # => 2D ndarray
+#
+# Choosing cmap: https://matplotlib.org/3.3.1/tutorials/colors/colormaps.html
+# Color codes: https://matplotlib.org/tutorials/colors/colors.html
