aa03a5e5d74fb273067efada71b2e8fd6d5ad54b
Patrick <patrick.n.raanes@gmail.com>
2020-10-13 15:01:16 +0200

    Misc

--- example_3.py
+++ example_3.py
@@ -8,18 +8,18 @@ The figure reveals the (relative) importance (in the EnKF) of
 localization and inflation.
 
 The code also demonstrates:
- - Parallelization (accross independent experiments) with mp=True/"GCP".
- - Data management with xpSpace: load, sub-select, print, plot.
+    - Parallelization (accross independent experiments) with mp=True/GCP.
+    - Data management with xpSpace: load, sub-select, print, plot.
 
 Note: unless you have a lot of CPUs available, you probably want to reduce
-      the number of experiments (by shortening the list of ``seed``
-      or some other variable) in the code below.
+    the number of experiments (by shortening the list of ``seed``
+    or some other variable) in the code below.
 
 Ref[1]: Book: "Data Assimilation: Methods, Algorithms, and Applications"
-        by M. Asch, M. Bocquet, M. Nodet.
-        http://books.google.no/books?id=FtDZDQAAQBAJ&q=figure+6.6
-        Alternative source: "Figure 5.7" of:
-        cerea.enpc.fr/HomePages/bocquet/teaching/assim-mb-en.pdf
+    by M. Asch, M. Bocquet, M. Nodet.
+    http://books.google.no/books?id=FtDZDQAAQBAJ&q=figure+6.6
+    Alternative source: "Figure 5.7" of:
+    cerea.enpc.fr/HomePages/bocquet/teaching/assim-mb-en.pdf
 """
 
 from dapper import *
@@ -82,44 +82,46 @@ savepath = xps.launch(HMM,sd0,__file__,mp=GCP)
 # Plot results
 ##############################
 # The following **only** uses saved data
-# => Can run as a separate script, where savepath is manually set.
+# => Can run as a separate script, where save_as is manually set.
 # For example, I have result-data stored at:
-# savepath = '~/dpr_data/example_3/run1'
+# save_as = '~/dpr_data/example_3/run1'
 
 # Load
-xps = load_xps(savepath)
+xps = load_xps(save_as)
 
 # Remove experiments we don't want to print/plot at the moment
-xps = [xp for xp in xps
-    if  getattr(xp,'upd_a'    ,None) != "PertObs"
+xps = [xp for xp in xps if True
+    # and getattr(xp,'upd_a'    ,None) != "PertObs"
     and getattr(xp,'da_method',None) != "EnKF_N"
     and 6 <= getattr(xp,'HMM_F',nan) <= 9
     ]
 
+# Print all
+# xpList(xps).print_avrgs(statkeys=["rmse.a","rmv.a"])
+
 # Associate each control variable with a coordinate/dimension
 xp_dict = xpSpace.from_list(xps)
 
-# Single-out certain settings
+# Single out (highlight) particular settings
 # Note: Must use infl=1.01 (not 1) to reproduce "no infl" scores in Ref[1],
 # as well as rot=True (better scores obtainable w/o rot).
 point_out = xp_dict.label_xSection
 point_out('NO-infl'    , ('infl'), da_method='LETKF', infl=1.01, rot=True)
-point_out('NO-infl-loc', ('infl'), da_method='EnKF' , infl=1.01, rot=True)
+point_out('NO-infl/loc', ('infl'), da_method='EnKF' , infl=1.01, rot=True)
 
-# Describe roles of coordinates/dimensions in plotting/printing
-axes_allotment=dict(inner="N", mean="seed", optim=('loc_rad','infl','rot'))
-
-# Print
-xp_dict.print("rmse.a", {**axes_allotment, "outer":"da_method"}, subcols=False)
+# Print, with columns: `inner`. Also try setting `outer=None`.
+tunable = ('loc_rad','infl','rot')
+axes = dict(outer="da_method",inner="N",mean="seed",optim=tunable)
+xp_dict.print("rmse.a", axes, subcols=False)
 
 # Plot -- try changing the axis allotments
 # (as suggested by the lines that are commented out).
 plt.ion()
-tabulated_data = xp_dict.plot('rmse.a', axes_allotment, # fignum=1,
+tabulated_data = xp_dict.plot('rmse.a', axes, # fignum=1,
     #     marker_axis="da_method", color_axis='infl', color_in_legend=False, 
     #     marker_axis='seed',                        marker_in_legend=False,
     #  linestyle_axis="rot",                      linestyle_in_legend=True,
          marker_axis='da_method',                   marker_in_legend=True,
     )
 # Custom adjustments
-beautify_fig_ex3(tabulated_data, savepath, xp_dict)
+beautify_fig_ex3(tabulated_data, save_as, xp_dict)
