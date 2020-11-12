b9102491b94e51c507ca3d4b676149a50e0496c6
patricknraanes <patrick.n.raanes@gmail.com>
2020-11-12 13:56:11 +0100

    Pep8 explore_props

--- dapper/mods/explore_props.py
+++ dapper/mods/explore_props.py
@@ -2,8 +2,8 @@
 
 An old version using explicit TLMs can be found in EmblAUS/Lyap_L{63,96}."""
 
-from dapper import *
 import dapper as dpr
+import dapper.tools.series as series
 from dapper.tools.math import with_rk4, with_recursion
 from dapper.tools.utils import progbar
 import dapper.tools.viz as viz
@@ -69,13 +69,13 @@ if mod == "L84":
 # In unitless time (as used here), this means LyapExps ∈ ( −4.87, 1.66 ) .
 if mod == "L96":
     from dapper.mods.Lorenz96.core import step
-    Nx  = 40            # State size (flexible). Usually 36 or 40
-    T   = 1e3           # Length of experiment (unitless time).
-    dt  = 0.1           # Step length
-    # dt = 0.0083       # Any dt<0.1 yield "almost correct" Lyapunov expos.
-    x0  = dpr.randn(Nx) # Init condition.
-    eps = 0.0002        # Ens rescaling factor.
-    N   = Nx            # Num of perturbations used.
+    Nx  = 40             # State size (flexible). Usually 36 or 40
+    T   = 1e3            # Length of experiment (unitless time).
+    dt  = 0.1            # Step length
+    # dt = 0.0083        # Any dt<0.1 yield "almost correct" Lyapunov expos.
+    x0  = dpr.randn(Nx)  # Init condition.
+    eps = 0.0002         # Ens rescaling factor.
+    N   = Nx             # Num of perturbations used.
 
 # Lyapunov exponents with F=10: [9.47   9.3    8.72 ..., -33.02 -33.61 -34.79] => n0:64
 if mod == "LUV":
@@ -88,7 +88,7 @@ if mod == "LUV":
     LUV.F = 10
     x0    = 0.01*dpr.randn(LUV.M)
     eps   = 0.001
-    N     = 66 # Don't need all Nx for a good approximation of upper spectrum.
+    N     = 66  # Don't need all Nx for a good approximation of upper spectrum.
 
 
 # Lyapunov exponents: [  0.08   0.07   0.06 ... -37.9  -39.09 -41.55]
@@ -110,26 +110,26 @@ if mod == "QG":
     # NB: There may arise an ipython/multiprocessing bug/issue.
     # Ref https://stackoverflow.com/a/45720872 . If so, set mp=False,
     # or run outside of ipython. However, I did not encounter lately.
-    model = model_config("sakov2008",{},mp=True)
+    model = model_config("sakov2008", {}, mp=True)
     step  = model.step
     Nx    = np.prod(shape)
-    ii    = np.random.choice(np.arange(Nx),100,False)
+    ii    = np.random.choice(np.arange(Nx), 100, False)
     T     = 1000.0
     dt    = model.prms['dtout']
     x0    = np.load(sample_filename)['sample'][-1]
-    eps   = 0.01 # ensemble rescaling
+    eps   = 0.01  # ensemble rescaling
     N     = 300
-
 
 
 ########################
 # Reference trajectory
 ########################
-t0 = 0.0 # NB: Arbitrary, coz models are autonom. But dont use nan coz QG doesn't like it.
-K  = int(round(T/dt))                      # Num of time steps.
-tt = np.linspace(dt,T,K)                   # Time seq.
-x  = with_recursion(step, prog="BurnIn")   (x0, int(10/dt), t0, dt)[-1]
-xx = with_recursion(step, prog="Reference")(x , K,          t0, dt)
+# NB: Arbitrary, coz models are autonom. But dont use nan coz QG doesn't like it.
+t0 = 0.0
+K  = int(round(T/dt))       # Num of time steps.
+tt = np.linspace(dt, T, K)  # Time seq.
+x  = with_recursion(step, prog="BurnIn")(x0, int(10/dt), t0, dt)[-1]
+xx = with_recursion(step, prog="Reference")(x, K, t0, dt)
 
 
 ########################
@@ -137,9 +137,12 @@ xx = with_recursion(step, prog="Reference")(x , K,          t0, dt)
 ########################
 # NB: Won't work with QG (too big, and BCs==0).
 fig, ax = dpr.freshfig(4)
-if "ii"    not in locals(): ii    = np.arange(min(100,Nx))
-if "nlags" not in locals(): nlags = min(100,K)
-ax.plot(tt[:nlags], np.nanmean( dpr.series.auto_cov(xx[:nlags,ii],L=nlags,corr=1), axis=1) )
+if "ii" not in locals():
+    ii = np.arange(min(100, Nx))
+if "nlags" not in locals():
+    nlags = min(100, K)
+ax.plot(tt[:nlags], np.nanmean(series.auto_cov(
+    xx[:nlags, ii], L=nlags, corr=1), axis=1))
 ax.set_xlabel('Time (t)')
 ax.set_ylabel('Auto-corr')
 viz.plot_pause(0.1)
@@ -148,28 +151,29 @@ viz.plot_pause(0.1)
 ########################
 # "Linearized" forecasting
 ########################
-LL = np.zeros((K,N))           # Local (in time) Lyapunov exponents
-E  = x + eps*np.eye(Nx)[:N] # Init E
+LL = np.zeros((K, N))        # Local (in time) Lyapunov exponents
+E  = x + eps*np.eye(Nx)[:N]  # Init E
 
-for k,t in enumerate(progbar(tt,"Ens (≈TLM)")):
+for k, t in enumerate(progbar(tt, "Ens (≈TLM)")):
     # if t%10.0==0: print(t)
 
-    x     = xx[k+1] # = step(x,t,dt)   # f.cast reference
-    E     = step(E,t,dt)               # f.cast ens (i.e. perturbed ref)
+    x      = xx[k+1]  # = step(x,t,dt)    # f.cast reference
+    E      = step(E, t, dt)               # f.cast ens (i.e. perturbed ref)
 
-    E     = (E-x).T/eps                # Compute f.cast perturbations
-    [Q,R] = sla.qr(E,mode='economic')  # Orthogonalize
-    E     = x + eps*Q.T                # Init perturbations  
-    LL[k] = np.log(np.abs(np.diag(R))) # Store local Lyapunov exponents
+    E      = (E-x).T/eps                  # Compute f.cast perturbations
+    [Q, R] = sla.qr(E, mode='economic')   # Orthogonalize
+    E      = x + eps*Q.T                  # Init perturbations
+    LL[k]  = np.log(np.abs(np.diag(R)))   # Store local Lyapunov exponents
 
 
 ########################
 # Running averages
 ########################
-running_LS = ( 1/tt[:,None] * np.cumsum(LL,axis=0) )
+running_LS = (1/tt[:, None] * np.cumsum(LL, axis=0))
 LS = running_LS[-1]
 print('Lyapunov spectrum estimate after t=T:')
-with np.printoptions(precision=2): print(LS)
+with np.printoptions(precision=2):
+    print(LS)
 n0 = sum(LS >= 0)
 print('n0  : ', n0)
 print('var : ', np.var(xx))
@@ -178,7 +182,7 @@ print('mean: ', np.mean(xx))
 ##
 
 fig, ax = dpr.freshfig(1)
-ax.plot(tt,running_LS,lw=1.2,alpha=0.7)
+ax.plot(tt, running_LS, lw=1.2, alpha=0.7)
 ax.set_title('Lyapunov Exponent estimates')
 ax.set_xlabel('Time')
 ax.set_ylabel('Exponent value')
