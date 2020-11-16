bd81dc2bf57900415a4ce92b1be06a04e25e65a1
patricknraanes <patrick.n.raanes@gmail.com>
2020-11-16 15:22:49 +0100

    Use _find_demos from dapper/__init__.py

--- tests/test_demos.py
+++ tests/test_demos.py
@@ -4,18 +4,10 @@ from importlib import import_module
 
 import dapper as dpr
 
-demos = []
-for d in (dpr.rc.dirs.dapper / "mods").iterdir():
-    x = d/"demo.py"
-    if x.is_file():
-        demos.append(x.relative_to(dpr.rc.dirs.DAPPER))
-
-
 plt.ion()
 
 
-@pytest.mark.parametrize(("path"), demos)
+@pytest.mark.parametrize(("path"), dpr._find_demos())
 def test_imp(path):
-    x = str(path.with_suffix("")).replace("/", ".")
-    import_module(x)
+    import_module(path)
     assert True
