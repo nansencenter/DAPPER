60fb1761263466fb06fb8bda357dc010167a849f
patricknraanes <patrick.n.raanes@gmail.com>
2020-11-20 16:11:02 +0100

    Misc style

--- example_3.py
+++ example_3.py
@@ -58,7 +58,7 @@ params = dict(
     rot      = [True, False],
     loc_rad  = dpr.round2([a*b for b in [.1, 1, 10] for a in [1, 2, 4, 7]]),
 )
-# Combines all of the params as suitable. Faster than "manual" for-loops.
+# Combines all the params suitable for a method. Faster than "manual" for-loops.
 for_params = dpr.get_param_setter(params, seed=3000+np.arange(10), F=[8, 10])
 
 xps = dpr.xpList()
