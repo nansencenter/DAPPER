e00051408b27207a0f02a4e245ef8c28789b153f
patricknraanes <patrick.n.raanes@gmail.com>
2020-12-17 14:57:03 +0100

    Misc setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,47 @@
 #!/usr/bin/env python3
+"""DAPPER benchmarks the performance of data assimilation (DA) methods.
+
+It is best to install from source,
+so that you the code is readily available to play with.
+
+See full README on [github](https://github.com/nansencenter/DAPPER).
+"""
 
 import os
 import re
+import sys
 
-import setuptools
+from setuptools import find_packages, setup
 
 # with open("README.md", "r") as fh:
 #     long_description = fh.read()
-long_description = (
-    "DAPPER is a set of templates for benchmarking"
-    " the performance of data assimilation (DA)."
-    " See full README at github.com/nansencenter/DAPPER.")
+DOCLINES = __doc__.split('\n')
 
+# Dependencies
+INSTALL_REQUIRES = [
+    'scipy>=1.1',
+    'ipython>=5.1',
+    'matplotlib~=3.1',  # >=3.1 to avoid Mac's framework-build issues
+    'tqdm~=4.31',
+    'pyyaml',
+    'ipdb',
+    'colorama~=0.4.1',
+    'tabulate~=0.8.3',
+    'dill==0.3.2',  # >=0.3.1.1 for dataclass. Pin vers. to equal GCP.
+    'multiprocessing-on-dill==3.5.0a4',
+    'threadpoolctl==1.0.0',
+]
 
-def filter_dirs(x):
-    val  = x != '__pycache__'
-    val &= x != '.DS_Store'
-    val &= x != 'pyks'
-    val &= not x.endswith('.py')
-    return val
+EXTRAS = {
+    'Qt':  ['PyQt5', 'qtpy'],
+    'dev': ['line_profiler', 'pdbpp', 'pre-commit'],
+    'test': ['pytest', 'pytest-cov', 'coverage',
+             'pytest-sugar', 'pytest-benchmark', 'pytest-clarity', 'pytest-xdist'],
+    'lint': ['flake8', 'flakehell', 'flake8-docstrings',
+             'flake8-bugbear', 'flake8-comprehensions'],
+    'build': ['twine', 'pdoc3'],
+}
+EXTRAS['dev'] += EXTRAS['test'] + EXTRAS['lint'] + EXTRAS['build']
 
 
 def find_version(*file_paths):
@@ -38,15 +61,21 @@ def find_version(*file_paths):
     raise RuntimeError("Unable to find version string.")
 
 
-setuptools.setup(
+# 'python setup.py publish' shortcut.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist bdist_wheel')
+    os.system('twine upload dist/*')
+    sys.exit(0)
 
+setup(
     # Basic meta
     name="DA-DAPPER",
     version=find_version("dapper", "__init__.py"),
     author="Patrick N. Raanes",
     author_email="patrick.n.raanes@gmail.com",
-    description=("Data Assimilation with Python:"
-                 " a Package for Experimental Research."),
+    description=DOCLINES[0],
+    long_description='\n'.join(DOCLINES[2:]),
+    long_description_content_type="text/markdown",
 
     # >= 3.5 (for @),
     # >=3.6 (for mpl==3.1),
@@ -54,44 +83,15 @@ setuptools.setup(
     # =3.8 (if you wish to use the the DAPPER/GCP cluster,
     #       since dill doesnt guarantee compat. accross versions).
     python_requires='>=3.8',
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS,
 
-    # Dependencies
-    install_requires=[
-        'scipy>=1.1',
-        'ipython>=5.1',
-        'matplotlib~=3.1',  # >=3.1 to avoid Mac's framework-build issues
-        'tqdm~=4.31',
-        'pyyaml',
-        'ipdb',
-        'colorama~=0.4.1',
-        'tabulate~=0.8.3',
-        'dill==0.3.2',  # >=0.3.1.1 for dataclass. Pin vers. to equal GCP.
-        # Multiprocessing:
-        'multiprocessing-on-dill==3.5.0a4',
-        'threadpoolctl==1.0.0',
-    ],
-    # Optional dependencies
-    extras_require={
-        'Qt':  ['PyQt5', 'qtpy'],
-        'Dev':  ['line_profiler', 'pdbpp',
-                 'pytest', 'pytest-cov', 'coverage',
-                 'flake8', 'pre-commit',
-                 'twine',
-                 'pdoc3'],
-    },
-    # File inclusion
-    # Note: find_packages() only works on __init__ dirs.
-    packages=setuptools.find_packages() +\
-    ['dapper.da_methods', 'dapper.tools', 'dapper.mods'] +\
-    ['dapper.mods.'+x for x in os.listdir('dapper/mods') if filter_dirs(x)] +\
-    ['dapper.mods.QG.f90'],
+    packages=find_packages(),
+    py_modules=["example_1", "example_2", "example_3"],
     package_data={
         '': ['*.txt', '*.md', '*.png', '*.yaml'],
-        'dapper.mods.QG.f90': ['*.txt', '*.md', '*.png',
-                               'Makefile', '*.f90'],
+        'dapper.mods.QG.f90': ['*.txt', '*.md', '*.png', 'Makefile', '*.f90'],
     },
-
-    py_modules=["example_1", "example_2", "example_3"],
 
     # Detailed meta
     classifiers=[
@@ -105,8 +105,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/nansencenter/DAPPER",
     keywords=("data-assimilation enkf kalman-filtering"
               " state-estimation particle-filter kalman"
