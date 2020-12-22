816e569470f0b53b325aaaa390b8d89ab82edc13
patricknraanes <patrick.n.raanes@gmail.com>
2020-12-22 19:21:12 +0100

    Bump version

--- dapper/__init__.py
+++ dapper/__init__.py
@@ -93,7 +93,7 @@ The rendered docstrings can be browsed
 through the following links, which are also available in the left sidebar.
 """
 
-__version__ = "1.0.8"
+__version__ = "1.1.0"
 
 import sys
 
