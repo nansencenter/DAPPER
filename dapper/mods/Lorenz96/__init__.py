545a7677cff90479d7de0409dceca017df98adba
patricknraanes <patrick.n.raanes@gmail.com>
2021-03-30 13:44:53 +0200

    Move non-essential Lorenz model stuff into extras.py

--- dapper/mods/Lorenz96/__init__.py
+++ dapper/mods/Lorenz96/__init__.py
@@ -12,8 +12,9 @@ Note: the implementation is `ndim`-agnostic.
 
 import numpy as np
 
-import dapper.tools.liveplotting as LP
-from dapper.mods.integration import integrate_TLM, rk4
+from dapper.mods.integration import rk4
+
+from .extras import LPs, dstep_dx
 
 __pdoc__ = {"demo": False}
 
@@ -35,38 +36,3 @@ def dxdt(x): return dxdt_autonomous(x) + Force
 
 def step(x0, t, dt):
     return rk4(lambda t, x: dxdt(x), x0, np.nan, dt)
-
-
-################################################
-# OPTIONAL (not required by EnKF or PartFilt):
-################################################
-def d2x_dtdx(x):
-    M = len(x)
-    F = np.zeros((M, M))
-    def md(i): return np.mod(i, M)  # modulo
-
-    for i in range(M):
-        F[i   , i]    = -1.0
-        F[i   , i-2]  = -x[i-1]
-        F[i, md(i+1)] = +x[i-1]
-        F[i   , i-1]  = x[md(i+1)]-x[i-2]
-
-    return F
-
-
-# dstep_dx = FD_Jac(step)
-def dstep_dx(x, t, dt):
-    # For L96, method='analytic' >> 'approx'
-    return integrate_TLM(d2x_dtdx(x), dt, method='analytic')
-
-
-################################################
-# Add some non-default liveplotters
-################################################
-def LPs(jj=None): return [
-    (1, LP.spatial1d(jj)),
-    (1, LP.correlations),
-    (0, LP.spectral_errors),
-    (0, LP.phase_particles(True, jj)),
-    (0, LP.sliding_marginals(jj)),
-]
