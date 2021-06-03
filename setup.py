b4267d494bd4d554bd2e3f617196a67ae2a26b00
patricknraanes <patrick.n.raanes@gmail.com>
2021-06-03 14:53:17 +0200

    Set mpl compatible version to 3.4.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ INSTALL_REQUIRES = [
     'ipython>=5.1',
     'jedi<0.18',  # ipython/issues/12740
     'jupyter',
-    'matplotlib~=3.1',  # >=3.1 to avoid Mac's framework-build issues
+    'matplotlib~=3.4.1',  # >=3.1 to avoid Mac's framework-build issues
     'mpl-tools==0.2.29',
     'tqdm~=4.31',
     'pyyaml',
