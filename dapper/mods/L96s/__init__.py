3da64735126adc7721469054d46342bb4e8dedfe
plinx <cgrudz@mailbox.org>
2021-06-15 17:27:35 -0700

    Second order Taylor scheme in rough draft form
    Initial simplifications have been introduced to homogenize the code with
    the rest of the repo.  The simple case of a Fourier truncation with p=1
    is emphasized, as this is all that matters for order 2.0 convergence.
    The general scheme discussed in the manuscript is not of particular
    interest for DA so we neglect this implementation, included in that
    manucript's repo.

--- dapper/mods/L96s/__init__.py
+++ dapper/mods/L96s/__init__.py
@@ -12,132 +12,24 @@
 import numpy as np
 from .extras import LPs, d2x_dtdx, dstep_dx
 
-__pdoc__ = {"demo": False}
-
-Force = 8.0
-Tplot = 10
-
 
 ########################################################################################################################
 ########################################################################################################################
 # Method definitions 
 ########################################################################################################################
 ########################################################################################################################
-# auxiliary functions for the 2nd order taylor expansion
-# these need to be computed once, only as a function of the order of truncation of the fourier series, p
+# basic Lorenz 96 defintions and functions
 
-def rho(p):
-        return 1.0/12.0 - 0.5 * np.pi**(-2) * np.sum(1.0 / np.arange(1, p+1)**2)
+__pdoc__ = {"demo": False}
 
-def alpha(p):
-        return (np.pi**2) / 180.0 - 0.5 * np.pi**(-2) * np.sum(1.0 / np.arange(1, p+1)**4)
+# energy injected into the system
+Force = 8.0
 
+# diffusion in the SDE process
+s = 0.05
 
-########################################################################################################################
-# 2nd order strong taylor SDE step
-
-def l96s_tay2_step(x, **kwargs):
-    """One step of integration rule for l96 second order taylor rule
-
-    The rho and alpha are to be computed by the auxiliary functions, depending only on p, and supplied for all steps.
-    This is the general formulation which includes, eg. dependence on the truncation of terms in the auxilliary
-    function C with respect to the parameter p.  In general, truncation at p=1 is all that is necessary for order
-    2.0 convergence, and in this case C below is identically equal to zero.  This auxilliary function can be removed
-    (and is removed) in other implementations for simplicity."""
-
-    # Infer system dimension
-    sys_dim = len(x)
-
-    dx_params = kwargs['dx_params']
-    h = kwargs['h']
-    diffusion = kwargs['diffusion']
-    dx_dt = kwargs['dx_dt']
-    p = kwargs['p']
-    rho = kwargs['rho']
-    alpha = kwargs['alpha']
-
-    # Compute the deterministic dxdt and the jacobian equations, squeeze at the end
-    # to eliminate the extra dimension from ensembles
-    dx = np.squeeze(l96(x, dx_params))
-
-    # x is always a single trajectory, we make into a 1 dimension array for the rest of code
-    x = np.squeeze(x)
-    Jac_x = l96_jacobian(x, dx_params)
-    ### random variables
-
-    # Vectors xi, mu, phi are sys_dim X 1 vectors of iid standard normal variables,
-    # zeta and eta are sys_dim X p matrices of iid standard normal variables. Functional relationships describe each
-    # variable W_j as the transformation of xi_j to be of variace given by the length of the time step h. The functions
-    # of random Fourier coefficients a_i, b_i are given in terms mu/ eta and phi/zeta respectively.
-
-    # draw standard normal samples
-    rndm = np.random.standard_normal([sys_dim, 2*p + 3])
-    xi = rndm[:, 0]
-
-    mu = rndm[:, 1]
-    phi = rndm[:, 2]
-
-    zeta = rndm[:, 3: p+3]
-    eta = rndm[:, p+3:]
-
-    ### define the auxiliary functions of random fourier coefficients, a and b
-
-    # denominators for the a series
-    tmp = np.tile(1 / np.arange(1, p+1), [sys_dim, 1])
-
-    # vector of sums defining a terms
-    a = -2 * np.sqrt(h * rho) * mu - np.sqrt(2*h) * np.sum(zeta * tmp, axis=1) / np.pi
-
-    # denominators for the b series
-    tmp = np.tile(1 / np.arange(1, p+1)**2, [sys_dim, 1])
-
-    # vector of sums defining b terms
-    b = np.sqrt(h * alpha) * phi + np.sqrt(h / (2 * np.pi**2) ) * np.sum(eta * tmp, axis=1)
-
-    # vector of first order Stratonovich integrals
-    J_pdelta = (h / 2) * (np.sqrt(h) * xi + a)
-
-
-    ### auxiliary functions for higher order stratonovich integrals ###
-
-    # the triple stratonovich integral reduces in the lorenz 96 equation to a simple sum of the auxiliary functions, we
-    # define these terms here abstractly so that we may efficiently compute the terms
-    def C(l, j):
-        C = np.zeros([p, p])
-        # we will define the coefficient as a sum of matrix entries where r and k do not agree --- we compute this by a
-        # set difference
-        indx = set(range(1, p+1))
-
-        for r in range(1, p+1):
-            # vals are all values not equal to r
-            vals = indx.difference([r])
-            for k in vals:
-                # and for row r, we define all columns to be given by the following, inexing starting at zero
-                C[r-1, k-1] = (r / (r**2 - k**2)) * ((1/k) * zeta[l, r-1] * zeta[j, k-1] + (1/r) * eta[l, r-1] * eta[j, k-1] )
-
-        # we return the sum of all values scaled by -1/2pi^2
-        return -.5 * np.pi**(-2) * np.sum(C)
-
-    def Psi(l, j):
-        # psi will be a generic function of the indicies l and j, we will define psi plus and psi minus via this
-        psi = h**2 * xi[l] * xi[j] / 3 + h * a[l] * a[j] / 2 + h**(1.5) * (xi[l] * a[j] + xi[j] * a[l]) / 4 \
-              - h**(1.5) * (xi[l] * b[j] + xi[j] * b[l]) / (2 * np.pi) - h**2 * (C(l,j) + C(j,l))
-        return psi
-
-    # we define the approximations of the second order Stratonovich integral
-    psi_plus = np.array([Psi((i-1) % sys_dim, (i+1) % sys_dim) for i in range(sys_dim)])
-    psi_minus = np.array([Psi((i-2) % sys_dim, (i-1) % sys_dim) for i in range(sys_dim)])
-
-    # the final vectorized step forward is given as
-    x_step = x + dx * h + h**2 * .5 * Jac_x @ dx    # deterministic taylor step
-    x_step += diffusion * np.sqrt(h) * xi           # stochastic euler step
-    x_step += + diffusion * Jac_x @ J_pdelta        # stochastic first order taylor step
-    x_step += diffusion**2 * (psi_plus - psi_minus) # stochastic second order taylor step
-
-    return np.reshape(x_step, [sys_dim, 1])
-
-########################################################################################################################
-
+# ?
+Tplot = 10
 
 def x0(M):
     return np.eye(M)[0]
@@ -157,3 +49,94 @@ def dxdt(x):
 
 def step(x0, t, dt):
     return rk4(lambda t, x: dxdt(x), x0, np.nan, dt)
+
+########################################################################################################################
+# Lorenz 96 Jacobian function
+
+def jacobian(x):
+    """"This computes the Jacobian of the Lorenz 96, for arbitrary dimension, equation about the point x."""
+
+    x_dim = len(x)
+
+    dxF = np.zeros([x_dim, x_dim])
+
+    for i in range(x_dim):
+        i_m_2 = np.mod(i - 2, x_dim)
+        i_m_1 = np.mod(i - 1, x_dim)
+        i_p_1 = np.mod(i + 1, x_dim)
+
+        dxF[i, i_m_2] = -x[i_m_1]
+        dxF[i, i_m_1] = x[i_p_1] - x[i_m_2]
+        dxF[i, i] = -1.0
+        dxF[i, i_p_1] = x[i_m_1]
+
+    return dxF
+
+
+########################################################################################################################
+# 2nd order strong taylor SDE step
+
+def l96s_tay2_step(x, t, dt):
+    """Steps forward state of L96s model by order 2.0 Taylor scheme
+
+    This is the basic formulation which makes a Fourier truncation at p=1 for the simple form of the order 2.0 method."""
+
+    # Infer system dimension
+    sys_dim = len(x)
+
+    # Compute the deterministic dxdt and the jacobian equations
+    dx = dxdt(x) 
+    dxF = jacobian(x)
+
+    # coefficients defined based on the p=1 Fourier truncation
+    rho = 1.0/12.0 - 0.5 * np.pi**(-2)
+    alpha =(np.pi**2) / 180.0 - 0.5 * np.pi**(-2) 
+
+    ### random variables
+    # Vectors xi, mu, phi are sys_dim vectors of iid standard normal variables,
+    # zeta and eta are sys_dim vectors of iid standard normal variables. Functional relationships describe each
+    # variable W_j as the transformation of xi_j to be of variance given by the length of the time step dt. 
+    # The functions of random Fourier coefficients a_i, b_i are given in terms mu/ eta and phi/zeta respectively.
+
+    # draw standard normal samples
+    rndm = np.random.standard_normal([sys_dim, 5])
+    xi = rndm[:, 0]
+
+    mu = rndm[:, 1]
+    phi = rndm[:, 2]
+
+    zeta = rndm[:, 3]
+    eta = rndm[:, 4]
+
+    ### define the auxiliary functions of random Fourier coefficients, a and b
+    # vector of "a" terms
+    a = -2.0 * np.sqrt(dt * rho) * mu - np.sqrt(2.0*dt) * zeta  / np.pi
+
+    # vector of "b" terms
+    b = np.sqrt(dt * alpha) * phi + np.sqrt(dt / (2.0 * np.pi**2) ) * eta 
+
+    # vector of first order Stratonovich integrals
+    J_pdelta = (dt / 2.0) * (np.sqrt(dt) * xi + a)
+
+    def Psi(l, j):
+        # psi will be a generic function of the indicies l and j, we will define psi plus and psi minus via this
+        psi = dt**2 * xi[l] * xi[j] / 3.0 + dt * a[l] * a[j] / 2.0 + dt**(1.5) * (xi[l] * a[j] + xi[j] * a[l]) / 4.0 \
+              - dt**(1.5) * (xi[l] * b[j] + xi[j] * b[l]) / (2.0 * np.pi) 
+        return psi
+
+    # we define the approximations of the second order Stratonovich integral
+    psi_plus = np.array([Psi((i-1) % sys_dim, (i+1) % sys_dim) for i in range(sys_dim)])
+    psi_minus = np.array([Psi((i-2) % sys_dim, (i-1) % sys_dim) for i in range(sys_dim)])
+
+    # the final vectorized step forward is given as
+    x  = x + dx * dt + dt**2 * 0.5 * dxF @ dx # deterministic taylor step
+    x += s * np.sqrt(dt) * xi                 # stochastic euler step
+    x += s * dxF @ J_pdelta                   # stochastic first order taylor step
+    x += s**2 * (psi_plus - psi_minus)        # stochastic second order taylor step
+
+    return x
+
+
+########################################################################################################################
+
+
