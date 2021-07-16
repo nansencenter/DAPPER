c22870ca97fe7f2faf40b19bac814d59ef367217
patricknraanes <patrick.n.raanes@gmail.com>
2021-07-16 18:52:19 +0200

    Dont label with tags

--- .github/push-docs.sh
+++ .github/push-docs.sh
@@ -13,8 +13,7 @@ sitemap() {
     echo "Sitemap: $WEBSITE/sitemap.txt" > 'robots.txt'
 }
 
-head=$(git rev-parse HEAD)
-
+# The docs were generated from master, but here we need the gh-pages branch
 git clone -b gh-pages "https://patricknraanes:$GH_PASSWORD@github.com/$GITHUB_REPOSITORY.git" gh-pages
 cp -R docs/* gh-pages/docs/
 cd gh-pages
@@ -30,5 +29,6 @@ if ! git config user.name; then
     git config user.email '41898282+github-actions[bot]@users.noreply.github.com'
 fi
 
-git commit -a -m "CI: Update docs for ${GITHUB_REF#refs/tags/} ($head)"
+head=$(git rev-parse --short HEAD)
+git commit -a -m "CI: Update docs for $head"
 git push
