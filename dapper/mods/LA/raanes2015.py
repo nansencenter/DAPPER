9138ec02614530bf517a2379c62e7c5d81286651
patricknraanes <patrick.n.raanes@gmail.com>
2021-07-16 18:29:56 +0200

    Add pdoc3 guard

--- dapper/mods/LA/raanes2015.py
+++ dapper/mods/LA/raanes2015.py
@@ -33,15 +33,16 @@ try:
     L = np.load(sample_filename)['Left']
 except FileNotFoundError:
     # First-time use
-    print('Did not find sample file', sample_filename,
-          'for experiment initialization. Generating...')
-    NQ        = 20000  # Must have NQ > (2*wnumQ+1)
-    A         = sinusoidal_sample(Nx, wnumQ, NQ)
-    A         = 1/10 * (A - A.mean(0)) / np.sqrt(NQ)
-    Q         = A.T @ A
-    U, s, _     = tsvd(Q)
-    L         = U*np.sqrt(s)
-    np.savez(sample_filename, Left=L)
+    if "pdoc" not in __import__("sys").modules:
+        print('Did not find sample file', sample_filename,
+              'for experiment initialization. Generating...')
+        NQ        = 20000  # Must have NQ > (2*wnumQ+1)
+        A         = sinusoidal_sample(Nx, wnumQ, NQ)
+        A         = 1/10 * (A - A.mean(0)) / np.sqrt(NQ)
+        Q         = A.T @ A
+        U, s, _     = tsvd(Q)
+        L         = U*np.sqrt(s)
+        np.savez(sample_filename, Left=L)
 
 X0 = modelling.GaussRV(C=modelling.CovMat(np.sqrt(5)*L, 'Left'))
 
