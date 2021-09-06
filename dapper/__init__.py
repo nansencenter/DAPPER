c3eaf03a0005619bd86c13846f80c228cc5b7d86
patricknraanes <patrick.n.raanes@gmail.com>
2021-09-06 14:25:28 +0200

    Exclude demo.py from root

--- dapper/__init__.py
+++ dapper/__init__.py
@@ -16,3 +16,22 @@ from .xp_launch import combinator, seed_and_simulate, xpList
 from .xp_process import (default_fig_adjustments, default_styles,
                          discretize_cmap, find_latest_run, load_xps,
                          make_label, rel_index, xpSpace)
+
+
+# Exclude "demo.py" files from documentation.
+# NB: pdoc complains/warns that it cannot find these files,
+# but it still works fine. https://github.com/pdoc3/pdoc/issues/206
+def _find_demos(as_path=False):
+    """Discover all model demo.py scripts."""
+    lst = []
+    for d in (rc.dirs.dapper/"mods").iterdir():
+        x = d/"demo.py"
+        if x.is_file():
+            x = x.relative_to(rc.dirs.DAPPER)
+            if not as_path:
+                x = str(x.with_suffix("")).replace("/", ".")
+            lst.append(x)
+    return lst
+
+
+__pdoc__ = {k: False for k in _find_demos()}
