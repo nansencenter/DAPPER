3dbc4674931917854947d7e837a451bb995704ad
patricknraanes <patrick.n.raanes@gmail.com>
2021-09-08 11:36:02 +0200

    Move docs to stats.md (allow getting rid of doctest: +SKIP)

--- dapper/stats.py
+++ dapper/stats.py
@@ -9,26 +9,7 @@ but after they have been averaged in time (after the assimilation has finished).
 Instances of these objects are created by `dapper.da_methods.da_method` objects
 and written to their `.stats` and `.avrgs` attributes.
 
-Only the time series created with `Stats.new_series` will be in the format
-operated on by `Stats.average_in_time`.  For example, create `ndarray` of
-length `KObs+1` to hold the time series of estimated inflation values:
->>> self.stats.new_series('infl', 1, KObs+1)  # doctest: +SKIP
-
-Alternatively you can overwrite a default statistic; for example:
->>> error_time_series = xx - ensemble_time_series.mean(axis=1)  # doctest: +SKIP
-... self.stats.err.rms.a = np.sqrt(np.mean(error_time_series**2, axis=-1))
-
-Of course, you could just do this
->>> self.stats.my_custom_stat = value  # doctest: +SKIP
-
-Moreover, recall that `xp.launch` (without `free=False`) will delete
-the `Stats` object from `xp` after the assimilation, in order to save memory.
-Therefore, in order to have `my_custom_stat` be available among `xp.avrgs`,
-it must be "registered":
->>> self.stats.stat_register.append("my_custom_stat")  # doctest: +SKIP
-
-Alternatively, you can do both at once
->>> self.stat("my_custom_stat", value)  # doctest: +SKIP
+.. include:: ../docs/stats.md
 """
 
 import warnings
---
+++ docs/stats.md
@@ -0,0 +1,21 @@
+Only the time series created with `Stats.new_series` will be in the format
+operated on by `Stats.average_in_time`.  For example, create `ndarray` of
+length `KObs+1` to hold the time series of estimated inflation values:
+>>> self.stats.new_series('infl', 1, KObs+1)
+
+Alternatively you can overwrite a default statistic; for example:
+>>> error_time_series = xx - ensemble_time_series.mean(axis=1)
+... self.stats.err.rms.a = np.sqrt(np.mean(error_time_series**2, axis=-1))
+
+Of course, you could just do this
+>>> self.stats.my_custom_stat = value
+
+Moreover, recall that `xp.launch` (without `free=False`) will delete
+the `Stats` object from `xp` after the assimilation, in order to save memory.
+Therefore, in order to have `my_custom_stat` be available among `xp.avrgs`,
+it must be "registered":
+>>> self.stats.stat_register.append("my_custom_stat")
+
+Alternatively, you can do both at once
+>>> self.stat("my_custom_stat", value)
+
