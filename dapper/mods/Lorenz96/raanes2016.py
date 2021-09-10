67179a20a4969df1fc41af7b0df98eaa7fdbbe01
patricknraanes <patrick.n.raanes@gmail.com>
2021-09-10 15:30:24 +0200

    Rename cntr --> DeCorr and center

--- dapper/mods/Lorenz96/raanes2016.py
+++ dapper/mods/Lorenz96/raanes2016.py
@@ -8,6 +8,6 @@ HMM.tseq = modelling.Chronology(0.01, dkObs=15, T=4**5, BurnIn=20)
 
 # from dapper.mods.Lorenz96.raanes2016 import HMM
 # xps += EnKS ('Sqrt',N=25,infl=1.08,rot=False,Lag=12)
-# xps += EnRTS('Sqrt',N=25,infl=1.08,rot=False,cntr=0.99)
+# xps += EnRTS('Sqrt',N=25,infl=1.08,rot=False,DeCorr=0.99)
 # ...
 # print_averages(xps,avrgs,[],['rmse.u'])
