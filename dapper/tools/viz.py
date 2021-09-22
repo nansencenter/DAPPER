1b2ae908ff073c0eacf4eb0ef5c2b065ea14254c
patricknraanes <patrick.n.raanes@gmail.com>
2021-09-22 13:48:20 +0200

    Misc, tiny, comment

--- dapper/tools/viz.py
+++ dapper/tools/viz.py
@@ -613,7 +613,7 @@ def default_fig_adjustments(tables, xticks_from_data=False):
             eval(f"ax.{direction}axis").set_major_formatter(sensible_f)
 
     # Set xticks
-    table0 = tables[:1][0]
+    table0 = tables[:1][0]  # get first table
     if xticks_from_data:
         ax = table0.panels[0]
         # Log-scale overrules any custom ticks. Restore control
