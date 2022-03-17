f9caa5f1c537c6e94f865e74ff749c0306d5a101
cgrudz <cgrudz@mailbox.org>
2022-03-17 10:29:44 -0700

    Updating and cleaning up time series script
    Making this more sensible for merge, fixing the no plotting issue by
    resizing the figure dimensions.

--- examples/generate_QG_time_series.py
+++ examples/generate_QG_time_series.py
@@ -1,4 +1,4 @@
-"""Truth-twin and observation time series for QG (quasi-geostrophic) model."""
+"""Stream function and observation time series for QG (quasi-geostrophic) model."""
 
 import numpy as np
 import scipy.ndimage.filters as filters
@@ -12,47 +12,36 @@ from dapper.mods.QG.sakov2008 import obs_inds
 # Auxiliary plotting function
 ###########
 
-def show(x0, psi=True, ax=None):
-    #
-    def psi_or_q(x):
-        return x if psi else compute_q(x)
-    #
+def show(x0, ax=None):
     if ax == None:
         fig, ax = plt.subplots()
 
-    im = ax.imshow(psi_or_q(square(x0)))
-
-    if psi:
-        im.set_clim(-30, 30)
-    else:
-        im.set_clim(-28e4, 25e4)
-
+    im = ax.imshow(square(x0))
+    im.set_clim(-30, 30)
+    
     def update(x):
-        im.set_data(psi_or_q(square(x)))
+        im.set_data(square(x))
     return update
 
 ###########
 # Main
 ###########
 
-## generate truth-twin and observation time series as per standard configuration
-## of Sakov2008, save the double time series
-xx, yy = HMM.simulate()
-
-# save data for easy re-use and debugging
-np.savez("QG_truth_obs_time_series.npz", xx=xx, yy=yy)
-
-# these lines can be used in place of above steps after first run,
-# comment out above
-tmp = np.load("QG_truth_obs_time_series.npz")
-xx = tmp['xx'][1:,]
-yy = tmp['yy']
-tmp.close()
+# try to load saved data 
+try:
+    tmp = np.load("QG_truth_obs_time_series.npz")
+    xx = tmp['xx'][1:]
+    yy = tmp['yy']
+    tmp.close()
+except:
+    # else, generate stream funciton and observation time series as per standard
+    # configuration  of Sakov2008, save the double time series
+    xx, yy = HMM.simulate()
+    np.savez("QG_truth_obs_time_series.npz", xx=xx, yy=yy)
 
 # generate observations on the same gridding as the state vector
-
 # initialize the storage on the parent state dimension
-yy_xx = np.empty(np.shape(xx))
+yy_xx = np.empty_like(xx)
 yy_xx[:] = np.NaN
 
 tseq = HMM.tseq
@@ -62,23 +51,22 @@ for k, ko, t, dt in pb.progbar(tseq.ticker, "Truth & Obs"):
         yy_xx[ko, indx] = yy[ko, :]
 
 # create figure
-fig, (ax1, ax2) = plt.subplots(ncols=2, sharex=True, sharey=True, figsize=(8, 4))
+fig, (ax1, ax2) = plt.subplots(ncols=2, sharex=True, sharey=True, figsize=(12, 6))
 for ax in (ax1, ax2):
     ax.set_aspect('equal', 'box')
-ax1.set_title(r'Truth-twin stream function: $\psi$')
+ax1.set_title(r'Stream function: $\psi$')
 ax2.set_title(r'Noisy observations: $\mathcal{H}(\psi) + \epsilon$')
 
-setter1 = show(xx[0], psi=True, ax=ax1)
-setter2 = show(yy_xx[0], psi=True, ax=ax2)
+setter1 = show(xx[0], ax=ax1)
+setter2 = show(yy_xx[0], ax=ax2)
 
-# create dual iterable for the animated plot of truth / obs
+# create dual iterable for the animated plot of stream function / obs
 ts = zip(xx, yy_xx)
 
 # run over the dual time series
-for k, val in pb.progbar(list(enumerate(ts)), "Animating"):
+for k, (xx, yy_xx) in pb.progbar(list(enumerate(ts)), "Animating"):
     if k % 2 == 0:
         fig.suptitle("k: "+str(k))
-        setter1(val[0])
-        setter2(val[1])
+        setter1(xx)
+        setter2(yy_xx)
         plt.pause(0.01)
-
