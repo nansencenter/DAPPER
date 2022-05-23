b24b4aacf77b2613de4b443dd7f765a19ccb4bf0
patnr <patrick.n.raanes@gmail.com>
2022-05-23 14:22:44 +0200

    Improve exclude of f90/py_mod

--- dapper/mods/QG/f90/__init__.py
+++ dapper/mods/QG/f90/__init__.py
@@ -1,3 +1,9 @@
 """Contains the Fortran-90 code of the model."""
 
-__pdoc__ = {"py_mod": False}
+from pathlib import Path
+
+# Exclude py_mod.*, e.g. "py_mod.cpython-38-x86_64-linux-gnu"
+__here__ = Path(__file__).parent
+excluded = __here__.glob("py_mod*")
+excluded = [str(x.relative_to(__here__).with_suffix("")) for x in excluded]
+__pdoc__ = {x: False for x in excluded}
