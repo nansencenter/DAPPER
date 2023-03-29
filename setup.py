cfd1b10dc5e773335a7399c4278b43bb48697a5a
patnr <patrick.n.raanes@gmail.com>
2023-03-29 16:24:51 +0200

    Require py>=3.9

--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ setup(
     # >=3.7 for dataclass, capture_output, dict ordering, np>=1.20.
     # ==3.7 for Colab
     # ==3.9 for the DAPPER/GCP cluster, since dill isn't compat. across versions.
-    python_requires='>=3.7',
+    python_requires='>=3.9',
     install_requires=INSTALL_REQUIRES,
     extras_require=EXTRAS,
 
