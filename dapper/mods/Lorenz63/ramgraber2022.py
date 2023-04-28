0d74e7db694befd89a63b000dece8df0e4ff38d1
patnr <patrick.n.raanes@gmail.com>
2023-04-28 19:36:42 +0200

    Add benchmarks from ensemble coupling/transport papers

---
+++ dapper/mods/Lorenz63/ramgraber2022.py
@@ -0,0 +1,37 @@
+"""Try settings of Figure 6A of Ramgraber's transport smoothing paper"""
+
+import numpy as np
+
+import dapper.mods as modelling
+from dapper.mods.Lorenz63 import Tplot, dstep_dx, step, x0
+
+tseq = modelling.Chronology(0.05, dto=.1, Ko=1000, Tplot=Tplot, BurnIn=4*Tplot)
+Nx = len(x0)
+Dyn = {
+    'M': Nx,
+    'model': step,
+    'linear': dstep_dx,
+    'noise': 0,
+}
+X0 = modelling.GaussRV(C=1, M=Nx)
+Obs = modelling.partial_Id_Obs(Nx, np.arange(Nx))
+Obs['noise'] = 4
+HMM = modelling.HiddenMarkovModel(Dyn, Obs, tseq, X0)
+
+
+####################
+# Suggested tuning
+####################
+
+# da_method       N   xN  reg   NER  |  rmse.a
+# -----------  ----  ---  ---  ----  -  ------
+# Climatology                        |    7.7
+# OptInterp                          |    1.72
+# Persistence                        |    5.2
+# Var3D                              |    1.16
+# EnKF_N          3  1               |    0.50
+# EnKF_N         10  1.5             |    0.43
+# iEnKS          10  1.3             |    0.32
+# PartFilt      100       2.4  0.3   |    0.39
+# PartFilt      800       0.9  0.2   |    0.28
+# PartFilt     4000       0.7  0.05  |    0.24
---
+++ dapper/mods/Lorenz96/spantini2019.py
@@ -0,0 +1,41 @@
+"""Try reproducing Figure 9 of Spantini's coupling/nonlinear-transport paper."""
+
+import numpy as np
+
+import dapper.mods as modelling
+from dapper.mods.Lorenz96 import Tplot, dstep_dx, step, x0
+from dapper.tools.localization import nd_Id_localization
+
+tseq = modelling.Chronology(0.02, dto=.4, Ko=2000, Tplot=Tplot, BurnIn=2*Tplot)
+Nx = 40
+x0 = x0(Nx)
+Dyn = {
+    'M': Nx,
+    'model': step,
+    'linear': dstep_dx,
+    'noise': 0,
+}
+X0 = modelling.GaussRV(mu=x0, C=.1)
+jj = np.arange(0, Nx, 2)
+Obs = modelling.partial_Id_Obs(Nx, jj)
+Obs['noise'] = .5
+Obs['localizer'] = nd_Id_localization((Nx,), (2,), jj)
+HMM = modelling.HiddenMarkovModel(Dyn, Obs, tseq, X0)
+
+####################
+# Suggested tuning
+####################
+
+#                                                      # rmse.a
+# xps += da.Climatology()                              # 3.6
+# xps += da.OptInterp()                                # 2.5
+# xps += da.Var3D(B="eye", xB=4)                       # 2.3
+# xps += da.EnKF ("Sqrt", N=40 , infl=1.10)            # 2.2
+# xps += da.EnKF ("Sqrt", N=60 , infl=1.10)            # 1.3
+# xps += da.EnKF ("Sqrt", N=100, infl=1.10)            # 0.97
+# xps += da.LETKF(        N=40 , infl=1.10, loc_rad=4) # 1.0
+# xps += da.LETKF(        N=60 , infl=1.02, loc_rad=4) # 0.89
+# xps += da.LETKF(        N=100, infl=1.02, loc_rad=4) # 0.84
+# xps += da.iEnKS("Sqrt", N=40 , infl=1.10, Lag=2)     # 0.52
+# xps += da.iEnKS("Sqrt", N=60 , infl=1.10, Lag=2)     # 0.47
+# xps += da.iEnKS("Sqrt", N=100, xN=1, Lag=2)          # 0.43
