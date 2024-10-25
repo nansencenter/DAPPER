26fd70bb2f5577bc595b21d5aa022fab708157a5
patnr <patrick.n.raanes@gmail.com>
2024-10-25 11:21:18 +0200

    Revert "Upper lim on Python"
    This reverts commit 4c66971766366a92477aba7d8c866ba9d8c7df5b.

--- setup.py
+++ setup.py
@@ -92,8 +92,7 @@ setup(
     # >=3.7 for dataclass, capture_output, dict ordering, np>=1.20.
     # ==3.7 for Colab
     # ==3.9 for the DAPPER/GCP cluster, since dill isn't compat. across versions.
-    # <3.11 because it failed with multiprocessing
-    python_requires='>=3.9,<3.11',
+    python_requires='>=3.9',
     install_requires=INSTALL_REQUIRES,
     extras_require=EXTRAS,
 
