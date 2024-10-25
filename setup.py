c4b6ceb549e0fb92619673d1cd52ce2e6f5afe92
patnr <patrick.n.raanes@gmail.com>
2024-10-25 11:15:25 +0200

    Rm deprecated

--- setup.py
+++ setup.py
@@ -96,7 +96,6 @@ setup(
     # >=3.7 for dataclass, capture_output, dict ordering, np>=1.20.
     # ==3.7 for Colab
     # ==3.9 for the DAPPER/GCP cluster, since dill isn't compat. across versions.
-    # <3.11 because it failed with multiprocessing
     python_requires=">=3.9",
     install_requires=INSTALL_REQUIRES,
     extras_require=EXTRAS,
