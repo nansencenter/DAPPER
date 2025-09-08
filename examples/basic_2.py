3eda48e1820dba6bf224b268a4c6e1cd22bcaed6
patnr <patnr@users.noreply.github.com>
2025-09-08 09:29:43 +0000

    Deployed 61a1a8e with MkDocs version: 1.6.1

---
+++ examples/basic_2.py
@@ -0,0 +1,83 @@
+# ## Multiple synthetic benchmark experiments
+
+# ### Imports
+# <b>NB:</b> If you're on <mark><b>Gooble Colab</b></mark>,
+# then replace `%matplotlib notebook` below by\
+# `!python -m pip install git+https://github.com/nansencenter/DAPPER.git`\
+# Also note that liveplotting does not work on Colab.
+
+# %matplotlib notebook
+import dapper as dpr  # noqa: I001
+import dapper.da_methods as da
+
+# ### DA method configurations
+
+
+# +
+from dapper.mods.Lorenz63.sakov2012 import HMM  # Expected rmse.a:
+
+xps = dpr.xpList()
+xps += da.Climatology()                                      # 7.6
+xps += da.OptInterp()                                        # 1.25
+#xps += da.Persistence()                                      # 10.7
+#xps += da.PreProg(lambda k, xx, yy: xx[k])                   # 0
+xps += da.Var3D(xB=0.1)                                      # 1.03
+xps += da.ExtKF(infl=90)                                     # 0.87
+xps += da.EnKF('Sqrt'   , N=3   , infl=1.30)                 # 0.82
+xps += da.EnKF('Sqrt'   , N=10  , infl=1.02, rot=True)       # 0.63
+xps += da.EnKF('PertObs', N=500 , infl=0.95, rot=False)      # 0.56
+xps += da.EnKF_N(         N=10             , rot=True)       # 0.54
+xps += da.iEnKS('Sqrt'  , N=10  , infl=1.02, rot=True)       # 0.31
+xps += da.PartFilt(       N=100 , reg=2.4  , NER=0.3)        # 0.38
+xps += da.PartFilt(       N=800 , reg=0.9  , NER=0.2)        # 0.28
+# xps += da.PartFilt(       N=4000, reg=0.7  , NER=0.05)       # 0.27
+# xps += da.PFxN(xN=1000,   N=30  , Qs=2     , NER=0.2)        # 0.56
+# -
+
+# #### With Lorenz-96 instead
+
+# +
+# from dapper.mods.Lorenz96.sakov2008 import HMM   # Expected rmse.a:
+# xps = dpr.xpList()
+# xps += da.Climatology()                                     # 3.6
+# xps += da.OptInterp()                                       # 0.95
+# xps += da.Var3D(xB=0.02)                                    # 0.41
+# xps += da.ExtKF(infl=6)                                     # 0.24
+# xps += da.EnKF('PertObs', N=40, infl=1.06)                  # 0.22
+# xps += da.EnKF('Sqrt'   , N=28, infl=1.02, rot=True)        # 0.18
+# # More sophisticated:
+# xps += da.EnKF_N(         N=24, rot=True)                   # 0.21
+# xps += da.EnKF_N(         N=24, rot=True, xN=2)             # 0.18
+# xps += da.iEnKS('Sqrt'  , N=40, infl=1.01, rot=True)        # 0.17
+# # With localisation:
+# xps += da.LETKF(          N=7 , infl=1.04, rot=True, loc_rad=4)  # 0.22
+# xps += da.SL_EAKF(        N=7 , infl=1.07, rot=True, loc_rad=6)  # 0.23
+# -
+
+# #### Other models (suitable xp's listed in HMM files):
+
+# +
+# from dapper.mods.LA           .evensen2009 import HMM
+# from dapper.mods.KS           .bocquet2019 import HMM
+# from dapper.mods.LotkaVolterra.settings101 import HMM
+# -
+
+# ### Launch
+
+# Write some more non-arg parameters to the `xps`. In this case we set the seed,
+# so that repeat experiments produce exactly the same result.
+
+for xp in xps:
+    xp.seed = 3000
+
+# Adjust experiment duration
+
+HMM.tseq.T = 50
+
+# Run/assimilate (for each `xp` in `xps`)
+
+save_as = xps.launch(HMM, liveplots=False)
+
+# ### Print results
+
+print(xps.tabulate_avrgs(statkeys=["err.rms.a", "crps.rms.a"]))
---
+++ examples/basic_3a.py
@@ -0,0 +1,99 @@
+# ## Illustrate usage of DAPPER to run MANY benchmark experiments.
+#
+# Launch many experiments (to explore a bunch of control variables),
+# and plot the compiled results as in a variety of ways.
+#
+# As an example, we will reproduce Figure 6.6 from reference [1].
+# The figure reveals the (relative) importance (in the EnKF) of
+# localization and inflation. The output of this script is shown here:
+# https://github.com/nansencenter/DAPPER#highlights
+#
+# The code also demonstrates:
+# - Parallelization (accross independent experiments) with mp=True/Google.
+# - Data management with xpSpace: load, sub-select, print, plot.
+#
+# NB: unless you have access to the DAPPER cluster, you probably want to reduce
+# the number of experiments by shortening the list of `seed`
+# (and maybe those of some tuning parameters) and/or reducing `Ko`.
+#
+# [1]: Asch, Bocquet, Nodet:
+#      "Data Assimilation: Methods, Algorithms, and Applications",
+#      Figure 6.6. Alternatively, see figure 5.7 of
+#      http://cerea.enpc.fr/HomePages/bocquet/teaching/assim-mb-en.pdf .
+#
+
+# #### Imports
+
+# %matplotlib notebook
+import numpy as np
+
+import dapper as dpr
+import dapper.da_methods as da
+
+# #### Hidden Markov Model
+
+from dapper.mods.Lorenz96.bocquet2015loc import HMM  # isort:skip
+
+
+def setup(hmm, xp):
+    """Experiment init.: Set Lorenz-96 forcing. Seed. Simulate truth/obs."""
+    import dapper as dpr  # req. on clusters
+    import dapper.mods.Lorenz96 as core
+
+    core.Force = xp.Force
+    return dpr.seed_and_simulate(hmm, xp)
+
+
+# This is shorter than ref. [1], but we also use repetitions (a seed list).
+HMM.tseq.Ko = 10**4
+
+# #### DA method configurations
+
+# Param ranges
+params = dict(
+    xB=[0.1, 0.2, 0.4, 1],
+    N=[5, 6, 7, 8, 9, 10, 12, 14, 16, 18, 20, 25, 30, 35, 40, 45, 50],
+    infl=1 + np.array([0, 0.01, 0.02, 0.04, 0.07, 0.1, 0.2, 0.4, 0.7, 1]),
+    rot=[True, False],
+    loc_rad=dpr.round2sigfig([a * b for b in [0.1, 1, 10] for a in [1, 2, 4, 7]], 2),
+)
+# Combines all the params suitable for a method. See doc for dpr.combinator.
+for_params = dpr.combinator(params, seed=3000 + np.arange(10), Force=[8, 10])
+
+xps = dpr.xpList()
+xps += for_params(da.Climatology)
+xps += for_params(da.OptInterp)
+xps += for_params(da.Var3D, B="eye")
+xps += for_params(da.EnKF, upd_a="PertObs")
+xps += for_params(da.EnKF, upd_a="Sqrt")
+xps += for_params(da.EnKF_N, infl=1.0)
+xps += for_params(da.LETKF)
+
+
+# #### Run experiments
+
+# Paralellize/distribute experiments across CPUs.
+mp = False  # 1 CPU only
+# mp = 7         # 7 CPUs (requires that you pip-installed DAPPER with [MP])
+# mp = True      # All CPUs
+# mp = "Google"  # Requires access to DAPPER cluster
+
+scriptname = "basic_3"  # since __file__ does not work in Jupyter
+save_as = xps.launch(HMM, scriptname, mp, setup=setup)
+
+
+# #### Print results
+
+# Load data
+if mp:
+    xps = dpr.load_xps(save_as)
+# Print as a flat list (as in basic_2.py)
+# print(dpr.xpList(xps).tabulate_avrgs(statkeys=["rmse.a","rmv.a"]))
+
+# Associate each control variable with a "coordinate"
+xp_dict = dpr.xpSpace.from_list(xps)
+
+# Print, split into tables by `outer` (also try None), and columns by `inner`.
+tunable = {"loc_rad", "infl", "xB", "rot"}
+dims = dict(outer="Force", inner="N", mean="seed", optim=tunable)
+xp_dict.print("rmse.a", dims, subcols=False)
